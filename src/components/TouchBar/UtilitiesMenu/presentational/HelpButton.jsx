2b441540caae0d454d02be406574f584316feb89
Lovisa Hassler <lovisa.hassler@liu.se>
2019-08-27 15:41:34 +0200

    Helpbutton refactored to use hooks and effects

--- src/components/TouchBar/UtilitiesMenu/presentational/HelpButton.jsx
+++ src/components/TouchBar/UtilitiesMenu/presentational/HelpButton.jsx
@@ -1,72 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Icon from '../../../common/MaterialIcon/MaterialIcon';
 import SmallLabel from '../../../common/SmallLabel/SmallLabel';
 
 import { instructionImage } from '../../../../api/resources';
 import styles from './../style/UtilitiesButtons.scss';
 
-class HelpButton extends Component {
-  constructor(props) {
-    super(props);
+const HelpButton = props => {
 
-    this.state = {
-      showImage: false,
-    };
+  const [showInstructions, setShowInstructions] = useState(false);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    this.timeout = 0;
-  }
-
-  componentWillUnmount() {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-      this.timeout = 0;
+  useEffect(() => {
+    if(showInstructions){
+      const timeout = setTimeout(() => {
+          setShowInstructions(false)
+      }, 9500); // match with animation-delay in UtilitiesButtons.scss
+      return () => { clearTimeout(timeout); };
     }
-  }
+  }, [showInstructions]);
 
-  hideImage() {
-    this.setState({
-      showImage: false,
-    });
+  return (
+    <div
+      className={`${styles.UtilitiesButton}
+      ${showInstructions && styles.active}`}
+      onClick={() => setShowInstructions(!showInstructions)}
+      role="button"
+      tabIndex="0"
+    >
+      <Icon icon="help_outline" className={styles.Icon} />
+      { showInstructions && <img src={instructionImage} className={styles.Instructions} alt={'instructions'} />}
+      <SmallLabel>Help</SmallLabel>
+    </div>
+  );
 
-    clearTimeout(this.timeout);
-    this.timeout = 0;
-  }
-
-  // Show image for 10s and then set state to false
-  handleClick() {
-
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-      this.timeout = 0;
-    }
-    this.setState({ showImage: !this.state.showImage });
-
-    if (!this.state.showImage) {
-      this.timeout = setTimeout(() => {
-          this.hideImage()
-      }, 9500); // match with animation-delay in .css
-    }
-  }
-
-  render() {
-    return (
-      <div
-        className={`${styles.UtilitiesButton}
-        ${this.state.showImage && styles.active}`}
-        onClick={this.handleClick}
-        role="button"
-        tabIndex="0"
-      >
-        <Icon icon="help_outline" className={styles.Icon} />
-        { this.state.showImage && <img src={instructionImage} className={styles.Instructions} alt={'instructions'} />}
-        <SmallLabel>Help</SmallLabel>
-      </div>
-    );
-  }
 }
 
 export default HelpButton;
