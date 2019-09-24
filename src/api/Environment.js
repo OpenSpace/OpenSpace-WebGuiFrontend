8342945fbfa20a9593b4549b7d80914400f0eb57
Emil Axelsson <emil.axelsson@liu.se>
2019-09-24 10:14:58 +0200

    Clarify dev vs prod mode

--- src/api/Environment.js
+++ src/api/Environment.js
@@ -1,3 +1,15 @@
+// In most cases, this file should not have to be changed.
+// Instead, the parameters should be overridden by
+// window.OpenSpaceEnvironment or window.DevelopmentEnvironment.
+
+// window.OpenSpaceEnvironment is set by a http request to `./environment.js`.
+// In production mode, this allows OpenSpace to serve a custom address and port through
+// the backend nodejs application.
+
+// window.DevelopmentEnvironment will automatically be set by the inclusion of
+// `devmode.js` which only happens when this application is hosted by the
+// webpack dev server, see webpack.config.js.
+
 const defaults = {
   wsAddress: 'localhost',
   wsPort: 4682,
