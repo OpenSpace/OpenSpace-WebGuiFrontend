e9c5d880af57e1fa80d92b34067b491f9e45d270
Ylva Selling <ylvse560@student.liu.se>
2021-04-15 16:21:49 +0200

    Add some ideas how to display the buttons for showing all images and only near images

--- src/components/BottomBar/WWTPanel.jsx
+++ src/components/BottomBar/WWTPanel.jsx
@@ -10,6 +10,7 @@ import ScrollOverlay from '../common/ScrollOverlay/ScrollOverlay';
 import FilterList from '../common/FilterList/FilterList';
 import SkybrowserFocusEntry from './Origin/SkybrowserFocusEntry';
 import propertyDispatcher from '../../api/propertyDispatcher';
+import Checkbox from '../common/Input/Checkbox/Checkbox';
 
 import {
   setPopoverVisibility,
@@ -35,9 +36,12 @@ class WWTPanel extends Component {
     super(props);
     this.state = {
       imageName: undefined,
+      showOnlyNearest: false,
     };
     this.togglePopover = this.togglePopover.bind(this);
     this.onSelect = this.onSelect.bind(this);
+    this.getAllImages = this.getAllImages.bind(this);
+    this.getNearestImages = this.getNearestImages.bind(this);
   }
 
   togglePopover() {
@@ -57,24 +61,38 @@ class WWTPanel extends Component {
     this.props.selectImage(identifier);
   }
 
+  getAllImages() {
+    if(this.props.systemList == null) return {};
+    let listArray = this.props.systemList.map( function(item, index) {
+      return {"name" : item["Name"] , "identifier": index.toString() , "key": index.toString(), "url": item["Thumbnail"] };
+    });
+    return listArray;
+  }
+
+  getNearestImages() {
+    if(this.props.systemList == null) return {};
+    let listArray = this.props.systemList.map( function(item, index) {
+      return {"name" : item["Name"] , "identifier": index.toString() , "key": index.toString(), "url": item["Thumbnail"] };
+    });
+    //return listArray;
+    return [{"name" : "Cool Cat" , "identifier": "0" , "key": "0", "url": "https://pbs.twimg.com/profile_images/3746761376/1adfb9f32c22458ffa418b6604a630c6.jpeg" }];
+  }
+
   get popover() {
     const imageNameLabel = <span>Image name</span>;
 
-    let imgData =  this.props.systemList != null ? this.props.systemList  : {}
-    let listArray = imgData.map( function(item, index) {
-      return {"name" : item["Name"] , "identifier": index.toString() , "key": index.toString(), "url": item["Thumbnail"] };
-    });
+    let imageList = this.state.showOnlyNearest ? this.getNearestImages() : this.getAllImages();
 
    let filterList = <FilterList
-     data={listArray}
+     data={imageList}
      className={styles.list}
-     searchText={"Search from " + listArray.length.toString() + " images..."}
+     searchText={"Search from " + imageList.length.toString() + " images..."}
      viewComponent={SkybrowserFocusEntry}
      onSelect={this.onSelect}
      active={this.state.imageName}
      searchAutoFocus
    />;
-
+   const textFormatLabel = <span>Show only near images</span>;
     return (
       <Popover
         className={Picker.Popover}
@@ -84,6 +102,22 @@ class WWTPanel extends Component {
         attached={true}
       >
         <div className={Popover.styles.content}>
+        <Picker onClick={() => this.setState({ showOnlyNearest: false })}>
+            <div>
+              <MaterialIcon className={styles.photoIcon} icon="list_alt" />
+            </div>
+          </Picker>
+        <Picker onClick={() => this.setState({ showOnlyNearest: true })}>
+            <div>
+              <MaterialIcon className={styles.photoIcon} icon="my_location" />
+            </div>
+          </Picker>
+            <Checkbox
+              checked={this.state.showOnlyNearest}
+              label={textFormatLabel}
+              setChecked={() => this.setState({ showOnlyNearest: ! this.state.showOnlyNearest })}
+            />
+
         {filterList}
         </div>
       </Popover>
