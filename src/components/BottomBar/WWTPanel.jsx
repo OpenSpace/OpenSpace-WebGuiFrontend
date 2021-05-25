e09514928f55e5dff01347a4cdde09aef8d19952
Ylva Selling <ylvse560@student.liu.se>
2021-05-25 10:29:42 +0200

    Add opacity slider to selected images in GUI

--- src/components/BottomBar/WWTPanel.jsx
+++ src/components/BottomBar/WWTPanel.jsx
@@ -15,7 +15,6 @@ import PopoverSkybrowser from '../common/Popover/PopoverSkybrowser';
 
 import {
   setPopoverVisibility,
-  selectImgSkyBrowser,
 } from '../../api/Actions';
 
 import {
@@ -38,7 +37,7 @@ class WWTPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageName: undefined,
+      imageName: "",
       showOnlyNearest: true,
       targetIsLocked: false,
       targetData: [{ra: 0, dec: 0}],
@@ -46,7 +45,7 @@ class WWTPanel extends Component {
       cameraData: {FOV : 70, RA: 0, Dec: 0},
     };
     this.togglePopover = this.togglePopover.bind(this);
-    this.onSelect = this.onSelect.bind(this);
+    this.selectImage = this.selectImage.bind(this);
     this.hoverOnImage = this.hoverOnImage.bind(this);
     this.getAllImages = this.getAllImages.bind(this);
     this.getNearestImages = this.getNearestImages.bind(this);
@@ -61,6 +60,7 @@ class WWTPanel extends Component {
     this.getImagesWith3Dcoord = this.getImagesWith3Dcoord.bind(this);
     this.createTargetBrowserPair = this.createTargetBrowserPair.bind(this);
     this.getSelectedTargetImages = this.getSelectedTargetImages.bind(this);
+    this.setOpacityOfImage = this.setOpacityOfImage.bind(this);
   }
 
   async componentDidMount(){
@@ -76,22 +76,21 @@ class WWTPanel extends Component {
     this.props.setPopoverVisibility(!this.props.popoverVisible)
   }
 
-  updateimageName(evt) {
-    this.setState({
-      imageName: evt.target.value
-    });
-  }
-
-  onSelect(identifier, evt) {
-    this.setState({
-      imageName: identifier,
-    });
-    this.props.luaApi.skybrowser.selectImage(Number(identifier));
+  selectImage(identifier) {
+    if(identifier) {
+      this.setState({
+        imageName: identifier,
+      });
+      this.props.luaApi.skybrowser.selectImage(Number(identifier));
+    }
     //this.props.luaApi.skybrowser.lockTarget(this.state.selectedTarget);
   }
 
   hoverOnImage(identifier) {
-    this.props.luaApi.skybrowser.moveCircleToHoverImage(Number(identifier));
+    if(identifier) {
+      this.props.luaApi.skybrowser.moveCircleToHoverImage(Number(identifier));
+    }
+
   }
 
   hoverLeavesImage() {
@@ -105,6 +104,11 @@ class WWTPanel extends Component {
   removeImageSelection(identifier) {
     this.props.luaApi.skybrowser.removeSelectedImageInBrowser(Number(identifier), this.state.selectedTarget);
   }
+
+  setOpacityOfImage(identifier, opacity) {
+    this.props.luaApi.skybrowser.setOpacityOfImageLayer(this.state.selectedTarget, Number(identifier), opacity);
+  }
+
 
   async getTargetData() {
     try {
@@ -127,14 +131,16 @@ class WWTPanel extends Component {
   }
 
   getSelectedTargetImages() {
-    let selectedImagesIndices = this.state.targetData[this.state.selectedTarget].selectedImages;
+    let selectedImagesIndices = this.state.targetData[this.state.selectedTarget];
 
     if(!selectedImagesIndices) {
       return [];
     }
     else {
       // For some reason, ghoul sends this vector as an object. Convert to array
-      let indices = Object.values(selectedImagesIndices);
+      let images = selectedImagesIndices.selectedImages;
+      if(!images) return [];
+      let indices = Object.values(images);
       return indices.map((index) => this.props.systemList[index.toString()]);
     }
   }
@@ -180,6 +186,8 @@ class WWTPanel extends Component {
 
   getNearestImages() {
     let targetPoint = this.state.targetData[this.state.selectedTarget];
+    if(!targetPoint) return [];
+
     let searchRadius = targetPoint.FOV / 2;
 
     let isWithinFOV = function (coord, target, FOV) {
@@ -237,7 +245,7 @@ class WWTPanel extends Component {
       viewComponent={SkybrowserFocusEntry}
       viewComponentProps={{"hoverFunc" : this.hoverOnImage, "hoverLeavesImage" : this.hoverLeavesImage,
         "currentTargetColor" : this.getCurrentTargetColor, "add3dImage" : this.add3dImage}}
-      onSelect={this.onSelect}
+      onSelect={this.selectImage}
       active={this.state.imageName}
       searchAutoFocus
       />;
@@ -250,13 +258,10 @@ class WWTPanel extends Component {
       currentTarget={this.state.selectedTarget.toString()}
       targetIsLocked={this.state.targetIsLocked}
       data={thisTabsImages}
-      onSelect={this.onSelect}
       viewComponent={SkybrowserFocusEntry}
       viewComponentProps={{"hoverFunc" : this.hoverOnImage, "hoverLeavesImage" : this.hoverLeavesImage,
       "lockTarget" : this.lockTarget , "unlockTarget" : this.unlockTarget, "createTargetBrowserPair" : this.createTargetBrowserPair,
-      "add3dImage" : this.add3dImage,  "removeImageSelection" : this.removeImageSelection }}
-      //onSelect={this.onSelect}
-      //active={this.state.imageName}
+      "add3dImage" : this.add3dImage,  "removeImageSelection" : this.removeImageSelection, "setOpacity": this.setOpacityOfImage, "onSelect":this.selectImage }}
       />;
 
   return (
@@ -344,11 +349,6 @@ const mapDispatchToProps = dispatch => ({
     dispatch(setPopoverVisibility({
       popover: 'skybrowser',
       visible
-    }));
-  },
-  selectImage: (imgName) => {
-    dispatch(selectImgSkyBrowser({
-      imgName
     }));
   },
 })
--- src/components/common/Tabs/SkybrowserTabs.jsx
+++ src/components/common/Tabs/SkybrowserTabs.jsx
@@ -108,7 +108,6 @@ class SkybrowserTabs extends Component {
                                 {...entry}
                                 {...this.props.viewComponentProps}
                                 key={entry.identifier}
-                                onSelect={this.props.onSelect}
                                 //active={this.props.active}
                                 />
                             ))}
@@ -129,7 +128,6 @@ SkybrowserTabs.propTypes = {
     data: PropTypes.array.isRequired,
     viewComponent: PropTypes.elementType,
     viewComponentProps: PropTypes.object,
-    onSelect: PropTypes.func,
     //active: PropTypes.any,
 
 };
@@ -138,7 +136,6 @@ SkybrowserTabs.defaultProps = {
     children: '',
     viewComponent: (props) => (<li>{ JSON.stringify(props) }</li>),
     viewComponentProps: {},
-    onSelect: () => {},
     //active: null,
 };
 
