afaaef0fcbd2beecaef7eb56512a165e44b41026
eriksunden <eriksunden85@gmail.com>
2022-02-22 09:16:16 +0100

    Added grouping and actions support in ToggleBoolButtons for TouchUI, as well as story_climate.json for example usage.

--- src/components/TouchBar/UtilitiesMenu/presentational/ToggleBoolButton.jsx
+++ src/components/TouchBar/UtilitiesMenu/presentational/ToggleBoolButton.jsx
@@ -2,37 +2,67 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import propertyDispatcher from '../../../../api/propertyDispatcher';
+import { triggerAction } from '../../../../api/Actions';
 import SmallLabel from '../../../common/SmallLabel/SmallLabel';
 import styles from './../style/UtilitiesButtons.scss';
 
 class ToggleBoolButton extends Component {
   constructor(props) {
     super(props);
+	
+	this.state = {
+      checked: false
+    };
 
     this.toggleProperty = this.toggleProperty.bind(this);
+	this.handleOnClick = this.handleOnClick.bind(this);
   }
 
   componentDidMount(){
-    this.props.boolPropertyDispatcher.subscribe();
+	if(this.props.property.isAction) {
+		this.props.boolPropertyDispatcher.subscribe();
+	}
   }
 
   componentWillUnMount(){
-    this.props.boolPropertyDispatcher.unsubscribe();
+	if(this.props.property.isAction) {
+		this.props.boolPropertyDispatcher.unsubscribe();
+	}
   }
 
   toggleProperty() {
-    const value = this.props.propertyNode.value ? false : true;
-    this.props.boolPropertyDispatcher.set(value);
+	if(this.props.property.isAction) {
+		if(!this.state.checked) {
+			this.props.triggerActionDispatcher(this.props.property.actionEnabled);
+			this.setState({ checked: true });
+		}
+		else {
+			this.props.triggerActionDispatcher(this.props.property.actionDisabled);
+			this.setState({ checked: false });
+		}
+	}
+	else {
+		const value = this.props.propertyNode.value ? false : true;
+		this.setState({ checked: value });
+		this.props.boolPropertyDispatcher.set(value);
+	}
+  }
+  
+  handleOnClick () {
+	  this.toggleProperty();
+	  if(this.props.property.group){
+		this.props.handleGroup(this.props);
+	  }
   }
 
   render() {
       return (
         <div
-          className={`${styles.UtilitiesButton} ${this.props.propertyNode.value === true && styles.active}`}
+          className={`${styles.UtilitiesButton} ${this.state.checked === true && styles.active}`}
           role="button"
           tabIndex="0"
           key={this.props.property.URI}
-          onClick={this.toggleProperty}
+          onClick={this.handleOnClick}
           id={this.props.property.URI}
         >
           <SmallLabel id={this.props.property.URI} style={{ textAlign: 'center' }}>
@@ -44,23 +74,32 @@ class ToggleBoolButton extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  let propertyNode = state.propertyTree.properties[ownProps.property.URI];
-
   // TODO (emmbr, 2022-01-18) Should check that the property node actually 
   // exists as well, and handle the case when it doesn't
+	
+  if(!ownProps.property.isAction) {
+	  let propertyNode = state.propertyTree.properties[ownProps.property.URI];
 
-  return {
-    propertyNode
-  };
+	  return{
+		propertyNode
+	  };
+  }
+  else
+	  return;
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   boolPropertyDispatcher : propertyDispatcher(dispatch, ownProps.property.URI),
+  triggerActionDispatcher: (action) => {
+    dispatch(triggerAction(action));
+  }
 });
 
 ToggleBoolButton = connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps, 
+  null, 
+  {forwardRef: true}
 )(ToggleBoolButton);
 
 ToggleBoolButton.propTypes = {
@@ -71,3 +110,4 @@ ToggleBoolButton.propTypes = {
 };
 
 export default ToggleBoolButton;
+
--- src/components/TouchBar/UtilitiesMenu/presentational/ToggleBoolButtons.jsx
+++ src/components/TouchBar/UtilitiesMenu/presentational/ToggleBoolButtons.jsx
@@ -5,12 +5,31 @@ import ToggleBoolButton from './ToggleBoolButton';
 class ToggleBoolButtons extends Component {
   constructor(props) {
     super(props);
+	this.handleGroup = this.handleGroup.bind(this);
+	this.toggleButtons =[];
+  }
+  
+  handleGroup (clickedProperty) {
+	  this.props.properties.map((property, i) => {
+        if(clickedProperty.property.URI != property.URI && clickedProperty.property.group == property.group){
+			this.toggleButtons[property.URI].toggleProperty();
+		}
+      })
   }
 
-  get propertiesButtons() {
-    return (this.props.properties.map((property, i) => 
-      <ToggleBoolButton key={property.URI} property={property} />
-    ));
+  get propertiesButtons(){
+    return (
+      this.props.properties.map((property, i) => {
+        return(
+          <ToggleBoolButton
+			  ref={ref => this.toggleButtons[property.URI] = ref}
+              key={property.URI}
+              property={property}
+			  handleGroup = {this.handleGroup} 
+          />
+        );
+      })
+    );
   }
 
 render() {
@@ -22,7 +41,7 @@ render() {
  };
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   const properties = state.storyTree.story.toggleboolproperties;
   return {
     properties
