130e58b708b7853fda8de36452457f51df6a48ac
Ylva Selling <ylva.selling@gmail.com>
2022-03-22 17:00:58 -0400

    Add buttons for adding and removing render copies

--- src/components/SkyBrowser/SkybrowserTabs.jsx
+++ src/components/SkyBrowser/SkybrowserTabs.jsx
@@ -310,7 +310,41 @@ class SkybrowserTabs extends Component {
           value={parseFloat(target.FOV.toFixed(precision))}
           placeholder="value 0"
         />
-        <Row className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
+        <Row>
+          { size.map((value, index) => (
+            <NumericInput
+              className={sizeData[index]}
+              label={sizeData[index]}
+              max={1}
+              min={0.1}
+              onValueChanged={(value) => {
+                const newSize = size.map(value => value * 2);
+                newSize[index] = value * 2;
+                skybrowserApi.setScreenSpaceSize(selectedBrowser, newSize[0], newSize[1]);
+              }}
+              step={0.1}
+              value={parseFloat(value.toFixed(precision))}
+              placeholder={`value ${index}`}
+            />
+          ))}
+        </Row>
+        <Row className={styles.buttonContainer}>
+          <Button
+            onClick={() => { skybrowserApi.addRenderCopy(selectedBrowser); }}
+            className={styles.renderCopyButton}
+            transparent
+          >
+            {"Add Copy"}
+          </Button>
+          <Button
+            onClick={() => { skybrowserApi.removeRenderCopy(selectedBrowser); }}
+            className={styles.renderCopyButton}
+            transparent
+          >
+            {"Remove Copy"}
+          </Button>
+        </Row>
+        <Row>
           <NumericInput
             className="Right Ascension"
             label="Right Ascension"
@@ -350,7 +384,7 @@ class SkybrowserTabs extends Component {
           setChecked={this.toggleFaceCamera}
           wide
         />
-        <Row className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
+        <Row>
           { colors.map((color, index) => (
             <NumericInput
               className={colorData[index]}
@@ -368,24 +402,7 @@ class SkybrowserTabs extends Component {
             />
           ))}
         </Row>
-        <Row className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
-          { size.map((value, index) => (
-            <NumericInput
-              className={sizeData[index]}
-              label={sizeData[index]}
-              max={1}
-              min={0.1}
-              onValueChanged={(value) => {
-                const newSize = size.map(value => value * 2);
-                newSize[index] = value * 2;
-                skybrowserApi.setScreenSpaceSize(selectedBrowser, newSize[0], newSize[1]);
-              }}
-              step={0.1}
-              value={parseFloat(value.toFixed(precision))}
-              placeholder={`value ${index}`}
-            />
-          ))}
-        </Row>
+
       </ScrollOverlay>
     );
   }
