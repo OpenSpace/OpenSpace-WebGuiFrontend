e8084b1a38d7ae9e581d414237c0b626e73a2913
Ylva Selling <ys5140@nyu.edu>
2022-04-04 12:10:17 -0400

    Add advanced settings for adding multiple render copies

--- src/components/SkyBrowser/SkybrowserSettings.jsx
+++ src/components/SkyBrowser/SkybrowserSettings.jsx
@@ -3,27 +3,44 @@ import PropTypes from 'prop-types';
 import Button from '../common/Input/Button/Button';
 import Row from '../common/Row/Row';
 import NumericInput from '../common/Input/NumericInput/NumericInput';
+import MaterialIcon from '../common/MaterialIcon/MaterialIcon';
 import Checkbox from '../common/Input/Checkbox/Checkbox';
 import ColorPickerPopup from '../common/ColorPicker/ColorPickerPopup';
+import ToggleContent from '../common/ToggleContent/ToggleContent';
 import TooltipSkybrowser from './TooltipSkybrowser';
-import styles from './SkybrowserTabs.scss';
+import Popover from '../common/Popover/Popover';
+import styles from './SkybrowserSettings.scss';
 
 class SkybrowserSettings extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showSettingsRaeInfo: false,
+      showExpandedCopies: false,
+      showExpandedSettings: false,
+      precisionLow: 2,
+      precisionHigh: 10,
+      newPosition: [2.1, 0, 0],
+      noOfCopies: 1,
+      showCopiesInfo: false,
     };
     this.valueToColor = this.valueToColor.bind(this);
     this.toggleFaceCamera = this.toggleFaceCamera.bind(this);
     this.toggleRadiusAzimuthElevation = this.toggleRadiusAzimuthElevation.bind(this);
     this.setRef = this.setRef.bind(this);
+    this.setExpandedCopies = this.setExpandedCopies.bind(this);
+    this.createRenderCopiesSection = this.createRenderCopiesSection.bind(this);
+    this.showExpandedCopySettings = this.showExpandedCopySettings.bind(this);
   }
 
   setRef(what) {
     return (element) => {
       this[what] = element;
     };
+  }
+
+  setExpandedCopies(expanded) {
+    this.setState({ showExpandedCopies: expanded });
   }
 
   toggleFaceCamera() {
@@ -59,6 +76,129 @@ class SkybrowserSettings extends Component {
     };
   }
 
+  showExpandedCopySettings() {
+    this.setState({ showExpandedSettings: !this.state.showExpandedSettings });
+  }
+
+  createRenderCopiesSection(target, api, skybrowserApi, selectedBrowser) {
+    const renderCopies = target.renderCopies;
+    const positionData = ['Radius', 'Azimuth', 'Elevation'];
+    const maxPosition = [10, 3.14, 3.14];
+    const minPosition = [0, -3.14, -3.14];
+    const newPositionVector = this.state.newPosition;
+
+    const renderCopiesButtons = renderCopies ? Object.values(renderCopies).map((entry, indexCopy) => {
+            return <Row key={indexCopy} className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
+              {entry.map((number, index) => {
+                return <NumericInput
+                  key={index}
+                  className={positionData[index]}
+                  label={positionData[index]}
+                  max={maxPosition[index]}
+                  min={minPosition[index]}
+                  onValueChanged={(newValue) => {
+                    const newVector = entry;
+                    newVector[index] = newValue;
+                    const renderCopyId = Object.keys(renderCopies)[indexCopy];
+                    const uriBrowser = `ScreenSpace.${selectedBrowser}.${renderCopyId}`;
+                    api.setPropertyValueSingle(uriBrowser, newVector);
+                  }}
+                  step={0.1}
+                  value={parseFloat((number).toFixed(this.state.precisionLow))}
+                  placeholder={`value ${index}`}
+                />;
+              })}
+            </Row>;
+          }) : "";
+
+    return (
+      <ToggleContent
+        title={"Display Copies"}
+        expanded={this.state.showExpandedCopies}
+        setExpanded={this.setExpandedCopies}
+      >
+        <ToggleContent
+          title={"Add Copy Settings"}
+          expanded={this.state.showExpandedSettings}
+          setExpanded={this.showExpandedCopySettings}
+        >
+          <NumericInput
+            label={"Number Of Copies"}
+            max={5}
+            min={1}
+            onValueChanged={(newValue) => {
+              this.setState({
+                noOfCopies : newValue
+              });
+            }}
+            step={1}
+            value={this.state.noOfCopies}
+            placeholder={`value`}
+          />
+          <Row><header>
+          Position for first copy
+          </header>
+          <MaterialIcon
+            icon={'info'}
+            ref={this.setRef('copiesPosition')}
+            onMouseEnter={() => this.setState({ showCopiesInfo : true})}
+            onMouseLeave={() => this.setState({ showCopiesInfo : false})}
+            style={{fontSize: '15px'}}>
+          </MaterialIcon>
+          {
+            this.state.showCopiesInfo && (
+              <TooltipSkybrowser placement="bottom-right" style={this.copiesPosition}>
+                {"This sets the position of the first copy. The additional copies will be evenly spread out on the Azimuth."}
+              </TooltipSkybrowser>
+            )
+          }
+          </Row>
+          <Row className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
+          {newPositionVector.map((number, index) => {
+              return <NumericInput
+                key={index}
+                className={positionData[index]}
+                label={positionData[index]}
+                max={maxPosition[index]}
+                min={minPosition[index]}
+                onValueChanged={(newValue) => {
+                  const newVector = this.state.newPosition;
+                  newVector[index] = newValue;
+                  this.setState({
+                    newPosition : newVector
+                  });
+                }}
+                step={0.1}
+                value={parseFloat((number).toFixed(this.state.precisionLow))}
+                placeholder={`value ${index}`}
+              />
+            })}
+          </Row>
+        </ToggleContent>
+        <Row className={styles.buttonContainer}>
+          <Button
+            onClick={() => {
+              skybrowserApi.addRenderCopy(selectedBrowser, this.state.noOfCopies, this.state.newPosition);
+            }}
+            className={styles.renderCopyButton}
+            transparent
+          >
+            Add
+          </Button>
+          <Button
+            onClick={() => {
+              skybrowserApi.removeRenderCopy(selectedBrowser);
+            }}
+            className={styles.renderCopyButton}
+            transparent
+          >
+            Remove
+          </Button>
+        </Row>
+        {renderCopies && renderCopiesButtons}
+      </ToggleContent>);
+  }
+
   render() {
       const { isUsingRae, isFacingCamera, selectedBrowser, target } = this.props;
       if (!target) {
@@ -69,61 +209,10 @@ class SkybrowserSettings extends Component {
       const colors = target.color;
       const colorData = ['Border Color: Red', 'Green', 'Blue'];
       const sizeData = ['Browser Width', 'Browser Height'];
-      const positionData = ['Radius', 'Azimuth', 'Elevation'];
-      const maxPosition = [10, 3.14, 3.14];
-      const minPosition = [0, -3.14, -3.14];
-      const precisionHigh = 10;
-      const precisionLow = 2;
       const { api } = this.props;
       const { skybrowserApi } = this.props;
-      const renderCopies = target.renderCopies;
 
-      const renderCopiesButtons = renderCopies ? Object.values(renderCopies).map((entry, indexCopy) => {
-              return <Row className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
-                {entry.map((number, index) => {
-                  return <NumericInput
-                    className={positionData[index]}
-                    label={positionData[index]}
-                    max={maxPosition[index]}
-                    min={minPosition[index]}
-                    onValueChanged={(newValue) => {
-                      const newVector = entry;
-                      newVector[index] = newValue;
-                      skybrowserApi.moveRenderCopy(selectedBrowser, indexCopy, newVector);
-                    }}
-                    step={0.1}
-                    value={parseFloat((number).toFixed(precisionLow))}
-                    placeholder={`value ${index}`}
-                  />;
-                })}
-              </Row>;
-            }) : "";
-
-      const renderCopiesSection = (
-        <div>
-          <Row className={styles.buttonContainer}>
-            <Button
-              onClick={() => {
-                skybrowserApi.addRenderCopy(selectedBrowser);
-              }}
-              className={styles.renderCopyButton}
-              transparent
-            >
-              Add Copy
-            </Button>
-            <Button
-              onClick={() => {
-                skybrowserApi.removeRenderCopy(selectedBrowser);
-              }}
-              className={styles.renderCopyButton}
-              transparent
-            >
-              Remove Copy
-            </Button>
-          </Row>
-          {renderCopies && renderCopiesButtons}
-        </div>);
-
+      const renderCopiesSection = this.createRenderCopiesSection(target, api, skybrowserApi, selectedBrowser);
       // TODO: Fix color picker
       const colorPicker = (
         <ColorPickerPopup
@@ -147,12 +236,13 @@ class SkybrowserSettings extends Component {
               skybrowserApi.setVerticalFov(selectedBrowser, fov);
             }}
             step={1}
-            value={parseFloat(target.FOV.toFixed(precisionHigh))}
+            value={parseFloat(target.FOV.toFixed(this.state.precisionHigh))}
             placeholder="value 0"
           />
           <Row>
             {size.map((value, index) => (
               <NumericInput
+                key = {index}
                 className={sizeData[index]}
                 label={sizeData[index]}
                 max={1}
@@ -162,7 +252,7 @@ class SkybrowserSettings extends Component {
                   skybrowserApi.setScreenSpaceSize(selectedBrowser, size[0], size[1]);
                 }}
                 step={0.1}
-                value={parseFloat((value * 0.5).toFixed(precisionLow))}
+                value={parseFloat((value * 0.5).toFixed(this.state.precisionLow))}
                 placeholder={`value ${index}`}
               />
             ))}
@@ -177,7 +267,7 @@ class SkybrowserSettings extends Component {
               onValueChanged={value => skybrowserApi.setEquatorialAim(selectedBrowser, value, target.dec)
               }
               step={0.1}
-              value={parseFloat(target.ra.toFixed(precisionHigh))}
+              value={parseFloat(target.ra.toFixed(this.state.precisionHigh))}
               placeholder="value 1"
             />
             <NumericInput
@@ -189,7 +279,7 @@ class SkybrowserSettings extends Component {
               onValueChanged={value => skybrowserApi.setEquatorialAim(selectedBrowser, target.ra, value)
               }
               step={0.1}
-              value={parseFloat(target.dec.toFixed(precisionHigh))}
+              value={parseFloat(target.dec.toFixed(this.state.precisionHigh))}
               placeholder="value 2"
             />
           </Row>
@@ -226,6 +316,7 @@ class SkybrowserSettings extends Component {
           <Row className={`${styles.vectorProperty} ${this.disabled ? styles.disabled : ''}`}>
             {colors.map((color, index) => (
               <NumericInput
+                key={index}
                 className={colorData[index]}
                 label={colorData[index]}
                 max={255}
