4ca12fab161593a2971178968ab298026be64685
Alexander Bock <alexander.bock@liu.se>
2022-04-05 14:53:39 +0200

    Manually set the parts of the Date class as the `parse` function does not handle all kinds of dates

--- src/api/Reducers/time.js
+++ src/api/Reducers/time.js
@@ -37,7 +37,55 @@ export const time = (state = defaultState, action = {}) => {
       const newState = {...state};
 
       if (time !== undefined) {
-        newState.time = new Date(dateStringWithTimeZone(time));
+        // The date constructor only accepts a small array of the different types that we
+        // might encounter. In particular it doesn't work with any year ranges that don't
+        // have exactly four digits.
+        // So we parse the date string manually and call the correct function on the date
+        // object instead
+
+        // The time string is in a form:
+        //  1. 1234-03-15T22:59:20.345
+        //  2.   44-01-30T12:00:00.000
+        //  3. 12345-03-15T22:59:20.345
+        //  4. -1234-03-15T22:59:20.345
+        //  5. -  44-01-30T12:00:00.000
+        //  6. -12345-03-15T22:59:20.345
+        console.log(time);
+        let date = new Date("2000-01-01T12:00:00.000Z");
+        let wholeParts = time.split('T');
+        console.assert(wholeParts.length === 2);
+
+        //
+        // Dealing with the year
+        let dayParts = wholeParts[0].split('-');
+        console.assert(dayParts.length === 3 || dayParts.length === 4);
+        // If the length is 4, we have a leading - meaning that the date is negative
+        if (dayParts.length === 4) {
+          console.assert(dayParts[0].trim() === '');
+          dayParts.shift();
+          date.setFullYear(-parseInt(dayParts[0]));
+        }
+        else {
+          date.setFullYear(parseInt(dayParts[0]));
+        }
+
+        date.setMonth(parseInt(dayParts[1]));
+        date.setDate(parseInt(dayParts[2]));
+
+
+        //
+        // Dealing with the time
+        let timeParts = wholeParts[1].split(':');
+        console.assert(timeParts.length === 3);
+        date.setHours(parseInt(timeParts[0]));
+        date.setMinutes(parseInt(timeParts[1]));
+        let secondsParts = timeParts[2].split('.');
+        date.setSeconds(parseInt(secondsParts[0]));
+        if (secondsParts.length > 0) {
+          date.setMilliseconds(parseInt(secondsParts[1]));
+        }
+
+        newState.time = date;
       }
       if (deltaTime !== undefined) {
         newState.deltaTime = deltaTime;
