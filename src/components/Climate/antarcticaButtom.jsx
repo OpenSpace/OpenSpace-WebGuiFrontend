d20edf07aba56a55cd182b1b32bf5b4770146950
Anne Olthoff <ao2461@nyu.edu>
2022-05-10 10:04:25 -0400

    antarticaButtom

--- src/components/Climate/antarcticaButtom.jsx
+++ src/components/Climate/antarcticaButtom.jsx
@@ -2,15 +2,26 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './Buttom.scss';
 import Icon from '../common/MaterialIcon/MaterialIcon';
-const AntarcticaButtom = ( props ) => (
+
+const AntarcticaButtom = ( {props} ) => (
   <div className={styles.antarctica} onClick={props} >
-    <h4>info: {props.info}</h4>
+    <h2>{props}</h2>
     <Icon icon="ac_unit" className={styles.Icon} />
   </div>
 );
 
 AntarcticaButtom.propTypes = {
-  antarctica: PropTypes.func.isRequired,
+  props: PropTypes.func.isRequired,
 };
 
 export default AntarcticaButtom;
+
+
+const Post = (props) => {
+  return (
+    <div>
+      <h2>{props.content}</h2>
+      <h4> username: {props.user}</h4>
+    </div>
+  );
+};
