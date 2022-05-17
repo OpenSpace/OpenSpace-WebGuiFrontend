798a8bf6e7fa009c7b6b34d64a8d03ccc390455d
Ylva Selling <ylva.selling@gmail.com>
2022-05-17 11:40:41 -0400

    Fix to comments on pull request

--- src/utils/propertyTreeHelpers.js
+++ src/utils/propertyTreeHelpers.js
@@ -112,7 +112,7 @@ export function removeLastWordFromUri(uri) {
 // Returns whether a property should be visible in the gui
 export function isPropertyVisible(properties, uri) {
   const property = properties[uri];
-  const visibility = properties['OpenSpaceEngine.Visibility'];
+  const visibility = properties['OpenSpaceEngine.PropertyVisibility'];
 
   const splitUri = uri.split('.');
   if (splitUri.length > 1) {
