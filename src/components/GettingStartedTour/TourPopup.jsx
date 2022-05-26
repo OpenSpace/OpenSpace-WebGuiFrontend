4c71bb9ae267d2598ac8786f0f56c4edee9c4f77
Ylva Selling <ylva.selling@gmail.com>
2022-05-26 10:34:34 -0400

    Add animated arrow and att more content to the content json file

--- src/components/GettingStartedTour/TourPopup.jsx
+++ src/components/GettingStartedTour/TourPopup.jsx
@@ -8,50 +8,82 @@ import styles from './TourPopup.scss';
 import contents from './GettingStartedContent.json'
 import Button from '../common/Input/Button/Button'
 
+function AnimatedArrow({ rotation = 0, position, ...props }) {
+  const arrowRef = React.useRef(null);
+
+  let degrees = 0;
+  switch (rotation) {
+    case 'right':
+      degrees = 0;
+      break;
+    case 'down':
+      degrees = 90;
+      break;
+    case 'left':
+      degrees = 180;
+      break;
+    case 'up':
+      degrees = 270;
+      break;
+    default:
+      degrees = rotation;
+      break;
+  }
+
+  React.useEffect(() => {
+    arrowRef.current.animate([
+      { left: '0' },
+      { left: '10px' },
+      { left: '0' }
+    ], {
+      duration: 700,
+      iterations: Infinity
+    });
+  }, [arrowRef])
+  
+  return (
+    <div
+      className={styles.icon}
+      style={{ transform: `rotate(${degrees}deg)`, bottom: `${position.y}px`, left: `${position.x}px` }}
+      {...props} >
+      <div ref={arrowRef} className={styles.arrow}></div>
+    </div>);
+}
+
 function TourPopup({ properties }) {
   const [currentSlide, setCurrentSlide] = React.useState(0);
 
   const content = contents[currentSlide];
   const property = properties[content.uri];
-  console.log(currentSlide);
+  const uriCondition = content.value === property?.value;
 
-  if (content.value === property?.value) {
-    setCurrentSlide(currentSlide + 1);
-  }
-
-  console.log(property);
-  console.log(content.uri);
-
-  return <ResizeableDraggable
-      default={{
-        x: content.position.x,
-        y: content.position.y,
-        width: content.size.width,
-        height: content.size.height,
-      }}
-      minWidth={300}
-      minHeight={200}
-    bounds="window"
-    className={styles.content}
-    >
-    <h1 className={styles.title}>{ content.title }</h1>
-    <p className={styles.text}>{content.text}</p>
-    <div className={styles.buttonContainer}>
+  return <>
+    <AnimatedArrow rotation={content.arrowRotation} position={content.arrowPosition}/>
+    <ResizeableDraggable
+        default={{
+          x: content.position.x,
+          y: content.position.y,
+          width: content.size.width,
+          height: content.size.height,
+        }}
+        minWidth={300}
+        minHeight={200}
+      bounds="window"
+      className={styles.content}
+      >
+      <h1 className={styles.title}>{ content.title }</h1>
+      <p className={styles.text}>{content.firstText}</p>
+      {uriCondition && <p className={styles.text}>{content.secondText}</p>}
+      <div className={styles.buttonContainer}>
         <Button
-          onClick={() => setCurrentSlide(currentSlide - 1)}
-          disabled={!(currentSlide > 0)}
-        >
-          Back
-        </Button>
-        <Button
-          style={{ alignSelf: 'flex-end', position: 'static' }}
           onClick={() => setCurrentSlide(currentSlide + 1)}
-          disabled={!(currentSlide < contents.length - 1)}
+          disabled={!(uriCondition && currentSlide < contents.length - 1)}
         >
-          Skip
+          Next
         </Button>
     </div>
-    </ResizeableDraggable>;
+    </ResizeableDraggable>
+    </>;
 }
 
 TourPopup.propTypes = {
