3842fcdc6b2cc8d4d6cb2ef19d5559c70037fc53
Ylva Selling <ylva.selling@gmail.com>
2022-08-17 15:49:24 -0400

    Make filterlist in geoposition panel a bit shorter

--- src/components/BottomBar/GeoPositionPanel.jsx
+++ src/components/BottomBar/GeoPositionPanel.jsx
@@ -212,7 +212,7 @@ function GeoPositionPanel({ refresh, luaApi, popoverVisible, setPopoverVisibilit
               : 
                 <FilterList
                   searchText={"Filter results..."}
-                  height={'235px'}
+                  height={'230px'}
                 >
                   <FilterListData>
                     {places?.map?.((place) => {
