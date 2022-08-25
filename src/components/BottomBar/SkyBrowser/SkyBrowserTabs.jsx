1dfec1be051cf68fbde11a20802ebd536b9e2929
Emma Broman <emmabroman740@gmail.com>
2022-08-25 11:17:53 +0200

    Remove redundant dot in info string

--- src/components/BottomBar/SkyBrowser/SkyBrowserTabs.jsx
+++ src/components/BottomBar/SkyBrowser/SkyBrowserTabs.jsx
@@ -342,9 +342,14 @@ class SkyBrowserTabs extends Component {
           browser={browsers[selectedBrowserId]}
           selectedBrowserId={selectedBrowserId}
           luaApi={luaApi}
-        />);
+        />
+      );
     } else if (data.length === 0) {
-      content = <CenteredLabel>There are no selected images in this sky browser.</CenteredLabel>;
+      content = (
+        <CenteredLabel>
+          There are no selected images in this sky browser
+        </CenteredLabel>
+      );
     } else {
       content = this.createImageList();
     }
