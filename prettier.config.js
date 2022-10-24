2c851139a369b532c82820ddecfe1f369b71e512
Ylva Selling <ylva.selling@gmail.com>
2022-10-24 10:52:05 -0400

    Add linting

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: "es5",
+  singleQuote: true,
+  semi: true,
+  jsxSingleQuote: true,
+  trailingComma: "none",
+  arrowParens: "always",
+  endOfLine: "auto"
+};
