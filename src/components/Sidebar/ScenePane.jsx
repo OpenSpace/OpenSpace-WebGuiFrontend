9f86ac02310226f7f757133d08ab21336452fbec
Ylva Selling <ylva.selling@gmail.com>
2022-11-21 14:06:18 -0500

    Rewrite Scene pane as functional component

--- src/components/Sidebar/ScenePane.jsx
+++ src/components/Sidebar/ScenePane.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import { ObjectWordBeginningSubstring } from '../../utils/StringMatchers';
 import subStateToProps from '../../utils/subStateToProps';
 import {FilterList, FilterListData, FilterListFavorites} from '../common/FilterList/FilterList';
@@ -10,78 +10,68 @@ import ContextSection from './ContextSection';
 import PropertyOwner from './Properties/PropertyOwner';
 import Group from './Group';
 import { isPropertyOwnerHidden } from '../../utils/propertyTreeHelpers';
-import Button from '../common/Input/Button/Button';
 import Tooltip from '../common/Tooltip/Tooltip';
 import MaterialIcon from '../common/MaterialIcon/MaterialIcon';
 import styles from './ScenePane.scss';
 import { InputButton } from '../common/FilterList/FilterList';
 import Checkbox from '../common/Input/Checkbox/Checkbox';
 
-class ScenePane extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showOnlyEnabled: false,
-      showSearchSettings: false
-    };
-  }
+function ScenePane({ propertyOwners, groups, matcher, onlyEnabledMatcher, closeCallback }) {
+  const [showOnlyEnabled, setShowOnlyEnabled] = React.useState(false);
+  const [showSearchSettings, setShowSearchSettings] = React.useState(false);
 
-  render() {
-    const entries = this.props.propertyOwners.map(uri => ({
-      key: uri,
-      uri: uri,
-      expansionIdentifier: 'scene-search/' + uri
-    }));
+  const entries = propertyOwners.map(uri => ({
+    key: uri,
+    uri: uri,
+    expansionIdentifier: 'scene-search/' + uri
+  }));
 
-    const favorites = this.props.groups.map(item => ({
-      key: item,
-      path: item,
-      expansionIdentifier: 'scene/' + item 
-    }));
+  const favorites = groups.map(item => ({
+    key: item,
+    path: item,
+    expansionIdentifier: 'scene/' + item 
+  }));
 
-    const { matcher, onlyEnabledMatcher } = this.props;
-    const settingsButton =<>
-      <InputButton
-        onClick={() => this.setState({ showSearchSettings: !this.state.showSearchSettings })}
-        className={styles.settings}
-      >
-        <MaterialIcon icon="settings" className="small" />
-      </InputButton>
-      {this.state.showSearchSettings &&
-        <Tooltip placement={'right'} className={styles.toolTip}>
-          <Checkbox
-            label="Show Only Enabled"
-            checked={this.state.showOnlyEnabled}
-            left={false}
-            disabled={false}
-            setChecked={() => this.setState({ showOnlyEnabled: !this.state.showOnlyEnabled })}
-            wide
-          />
-        </Tooltip>}
-    </>;
+  const settingsButton = <>
+    <InputButton
+      onClick={() => this.setState({ showSearchSettings: !showSearchSettings })}
+      className={styles.settings}
+    >
+      <MaterialIcon icon="settings" className="small" />
+    </InputButton>
+    {showSearchSettings &&
+      <Tooltip placement={'right'} className={styles.toolTip}>
+        <Checkbox
+          label="Show Only Enabled"
+          checked={showOnlyEnabled}
+          left={false}
+          disabled={false}
+          setChecked={() => this.setState({ showOnlyEnabled: !showOnlyEnabled })}
+          wide
+        />
+      </Tooltip>}
+  </>;
 
-    return (
-      <Pane title="Scene" closeCallback={this.props.closeCallback}>
-        { (entries.length === 0) && (
-          <LoadingBlocks className={Pane.styles.loading} />
-        )}
-        {entries.length > 0 && (
-          <>
-
-            <FilterList matcher={this.state.showOnlyEnabled ? onlyEnabledMatcher : matcher} customButton={settingsButton} >
-              <FilterListFavorites>
-                <ContextSection expansionIdentifier="context" />
-                {favorites.map(favorite => <Group {...favorite} />)}
-              </FilterListFavorites>
-              <FilterListData>
-                {entries.map(entry => <PropertyOwner {...entry} />)}
-              </FilterListData>
-            </FilterList> 
-          </>
-        )}
-      </Pane>
-    );
-  }
+  return (
+    <Pane title="Scene" closeCallback={closeCallback}>
+      { (entries.length === 0) && (
+        <LoadingBlocks className={Pane.styles.loading} />
+      )}
+      {entries.length > 0 && (
+        <>
+          <FilterList matcher={showOnlyEnabled ? onlyEnabledMatcher : matcher} customButton={settingsButton} >
+            <FilterListFavorites>
+              <ContextSection expansionIdentifier="context" />
+              {favorites.map(favorite => <Group {...favorite} />)}
+            </FilterListFavorites>
+            <FilterListData>
+              {entries.map(entry => <PropertyOwner {...entry} />)}
+            </FilterListData>
+          </FilterList> 
+        </>
+      )}
+    </Pane>
+  );
 }
 
 ScenePane.propTypes = {
