09344a9022142451fc77cf25ab6fc98c3345bb19
Ylva Selling <ylva.selling@gmail.com>
2022-11-21 14:06:02 -0500

    Only show enabled propertyowners

--- src/components/Sidebar/Group.jsx
+++ src/components/Sidebar/Group.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { setPropertyTreeExpansion } from '../../api/Actions';
 import { sortGroups } from '../../api/keys';
 import ToggleContent from '../common/ToggleContent/ToggleContent';
@@ -8,6 +8,10 @@ import PropertyOwner, {
   displayName as propertyOwnerName, 
   nodeExpansionIdentifier as propertyOwnerNodeExpansionIdentifier 
 } from './Properties/PropertyOwner';
+
+const isEnabled = (properties, uri) => {
+  return properties[`${uri}.Renderable.Enabled`]?.value;
+};
 
 const displayName = path => {
   const splitPath = path.split('/');
@@ -38,28 +42,37 @@ const Group = ({ path, expansionIdentifier, autoExpand }) => {
       isExpanded = autoExpand;
     }
     return isExpanded;
-  });
+  }, shallowEqual);
 
   const entries = useSelector((state) => {
     const data = state.groups[path] || {};
     const owners = data.propertyOwners || [];
     const subGroups = data.subgroups || [];
-    const result = subGroups.map(g => ({
+    
+    // Filter PropertyOwners
+    const enabledOwners = owners.filter((owner) =>
+      isEnabled(state.propertyTree.properties, owner)
+    );
+    // Extract propertyOwners
+    const propertyOwners = enabledOwners.map(owner => ({
+        type: 'propertyOwner',
+        payload: owner,
+        name: propertyOwnerName(state.propertyTree.propertyOwners, state.propertyTree.properties, owner)
+    }));
+    // Extract groups
+    const groups = subGroups.map(subGroup => ({
       type: 'group',
-      payload: g,
-      name: displayName(g)
-    })).concat(owners.map(o => ({
-      type: 'propertyOwner',
-      payload: o,
-      name: propertyOwnerName(state.propertyTree.propertyOwners, state.propertyTree.properties, o)
-    })));
-    return result;
-  });
-
+      payload: subGroup,
+      name: displayName(subGroup)
+    }))
+    console.log(groups)
+    return groups.concat(propertyOwners);
+  }, shallowEqual);
+  
+  const hasEntries = entries.length !== 0;
   const pathFragments = path.split('/');
   const groupName = pathFragments[pathFragments.length - 1];
-  const result = sortGroups[groupName];
-  const sortOrdering = result;
+  const sortOrdering = sortGroups[groupName];
 
   const dispatch = useDispatch();
 
@@ -70,7 +83,7 @@ const Group = ({ path, expansionIdentifier, autoExpand }) => {
     }));
   };
 
-  var sortedEntries = entries.sort((a, b) =>
+  let sortedEntries = entries.sort((a, b) =>
     a.name.localeCompare(b.name, 'en')
   );
 
@@ -84,7 +97,7 @@ const Group = ({ path, expansionIdentifier, autoExpand }) => {
       });
   }
 
-  return <ToggleContent
+  return hasEntries && <ToggleContent
     title={displayName(path)}
     expanded={isExpanded}
     setExpanded={setExpanded}
