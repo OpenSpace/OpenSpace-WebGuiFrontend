3fb0e2bd86a8a99ba5a6e1ebb863517a4574b45a
Emma Broman <emma.broman@liu.se>
2023-02-09 18:19:12 +0100

    oops

--- src/components/GettingStartedTour/TourPopup.jsx
+++ src/components/GettingStartedTour/TourPopup.jsx
@@ -303,7 +303,7 @@ function TourPopup({ setVisibility, isVisible }) {
             <img src={openspaceLogo} className={styles.logo} />
           </div>}
         <p className={styles.text}>{content.firstText}</p>
-        <Goal text={content} setIsFulfilled={setIsFulfilled} />
+        <Goal content={content} setIsFulfilled={setIsFulfilled} />
         {<p className={` ${styles.text} ${styles.infoText}`} style={ isFulfilled ? { paddingTop: '40px' } : undefined }>{content.infoText}</p>}
         {content.showMouse && !isFulfilled &&  
         <div className={ styles.scroll } >
