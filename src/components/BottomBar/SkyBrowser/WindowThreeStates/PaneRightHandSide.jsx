71f643d84cfcefd4247b17be3ca474224de26792
Ylva Selling <ylva.selling@gmail.com>
2023-03-22 10:55:31 -0400

    Cleanup PaneRightHandSide

--- src/components/BottomBar/SkyBrowser/WindowThreeStates/PaneRightHandSide.jsx
+++ src/components/BottomBar/SkyBrowser/WindowThreeStates/PaneRightHandSide.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Resizable } from 're-resizable';
 import styles from './WindowThreeStates.scss';
-import MaterialIcon from '../../../common/MaterialIcon/MaterialIcon';
-import Button from '../../../common/Input/Button/Button';
 
 class PaneRightHandSide extends Component {
   constructor(props) {
