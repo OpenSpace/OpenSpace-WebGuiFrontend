cc0bc8b2a30e826e24a9fac3c00ed451fa363af5
Ylva Selling <ylva.selling@gmail.com>
2023-05-17 16:54:54 -0400

    Lint fix SvgIcon

--- src/components/common/SvgIcon/SvgIcon.jsx
+++ src/components/common/SvgIcon/SvgIcon.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function SvgIcon(props) {
-  return <div {...props} style={{ ...props.style, display: 'inline-grid', width: '1em' }}>{ props.children }</div>;
+function SvgIcon({ style, children, ...props }) {
+  return (
+    <div
+      style={{ style, display: 'inline-grid', width: '1em' }}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default SvgIcon;
