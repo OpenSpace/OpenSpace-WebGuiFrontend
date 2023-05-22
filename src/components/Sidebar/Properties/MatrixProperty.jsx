f77200502b7687904a11f678fe518c809439b4f1
Ylva Selling <ylva.selling@gmail.com>
2023-05-22 17:31:02 -0400

    Lint fix Matrix property

--- src/components/Sidebar/Properties/MatrixProperty.jsx
+++ src/components/Sidebar/Properties/MatrixProperty.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import { copyTextToClipboard } from '../../../utils/helpers';
-import InfoBox from '../../common/InfoBox/InfoBox';
 import NumericInput from '../../common/Input/NumericInput/NumericInput';
 import Row from '../../common/Row/Row';
 
@@ -15,16 +13,16 @@ class MatrixProperty extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
-  get disabled() {
-    return this.props.description.MetaData.isReadOnly;
-  }
-
   onChange(index) {
     return (newValue) => {
       const stateValue = this.props.value;
       stateValue[index] = parseFloat(newValue);
       this.props.dispatcher.set(stateValue);
     };
+  }
+
+  get disabled() {
+    return this.props.description.MetaData.isReadOnly;
   }
 
   render() {
@@ -34,9 +32,9 @@ class MatrixProperty extends Component {
     } = description.AdditionalData;
     const firstLabel = <PropertyLabel description={description} />;
 
-    const values = value.map((value, index) => ({
+    const values = value.map((val, index) => ({
       key: `${description.Name}-${index}`,
-      value: parseFloat(value),
+      value: parseFloat(val),
       index
     }));
     // Find N
@@ -47,9 +45,8 @@ class MatrixProperty extends Component {
     // eslint-disable react/no-array-index-key
     return (
       <div className={`${styles.matrixProperty} ${this.disabled ? styles.disabled : ''}`}>
-        >
-        { groups.map((group, index) => (
-          <Row key={`row-${index}`}>
+        { groups.map((group) => (
+          <Row key={`row-${group[0].key}`}>
             { group.map((comp) => (
               <NumericInput
                 inputOnly
