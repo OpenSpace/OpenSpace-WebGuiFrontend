823c797457de3160799238e9aee6b754e8ef03da
Ylva Selling <ylva.selling@gmail.com>
2023-05-23 16:35:52 -0400

    Lint fix NodeMetaContainer; rewrite as functional component; switch from connect to useSelector

--- src/components/NodeMetaPanel/NodeMetaContainer.jsx
+++ src/components/NodeMetaPanel/NodeMetaContainer.jsx
@@ -1,42 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import NodeMetaPanel from './NodeMetaPanel';
 
 import styles from '../NodePropertiesPanel/NodePopOverContainer.scss';
 
-class NodeMetaContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
+function NodeMetaContainer() {
+  const activeNodeMetaPanels = useSelector((state) => {
+    const panels = state.local.popovers.activeNodeMetaPanels;
+    return [...Object.keys(panels)];
+  });
 
-  render() {
-    const { activeNodeMetaPanels } = this.props;
-    return (
-      <div className={styles.NodePopOverContainer}>
-        { activeNodeMetaPanels.map((uri) => (
-          <NodeMetaPanel
-            uri={uri}
-            key={uri}
-          />
-        )) }
+  return (
+    <div className={styles.NodePopOverContainer}>
+      { activeNodeMetaPanels.map((uri) => (
+        <NodeMetaPanel
+          uri={uri}
+          key={uri}
+        />
+      )) }
 
-      </div>
-    );
-  }
+    </div>
+  );
 }
-
-const mapStateToProps = (state) => {
-  const { activeNodeMetaPanels } = state.local.popovers;
-  const panels = Object.keys(activeNodeMetaPanels).map((key) => key);
-
-  return {
-    activeNodeMetaPanels: panels
-  };
-};
-
-NodeMetaContainer = connect(
-  mapStateToProps,
-)(NodeMetaContainer);
 
 export default NodeMetaContainer;
