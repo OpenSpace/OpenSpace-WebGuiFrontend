4d138e28708b612c287de860cf6c0f863f36b5a9
Ylva Selling <ylva.selling@gmail.com>
2023-05-23 17:23:13 -0400

    Extract MouseDescriptions to another file from TourPopup

--- src/components/GettingStartedTour/TourPopup.jsx
+++ src/components/GettingStartedTour/TourPopup.jsx
@@ -6,78 +6,11 @@ import Button from '../common/Input/Button/Button';
 import Checkbox from '../common/Input/Checkbox/Checkbox';
 import MaterialIcon from '../common/MaterialIcon/MaterialIcon';
 import Goal from './Goal';
+import MouseDescriptions from './MouseDescriptions';
 import contents from './GettingStartedContent.json';
 import openspaceLogo from './openspace-color-transparent.png';
 
 import styles from './TourPopup.scss';
-
-function KeyboardButton({ buttonText, ...props }) {
-  return (
-    <div className={`${styles.keyboardButton} ${styles.centerContent}`} {...props}>
-      <p className={styles.keyboardButtonText}>{buttonText}</p>
-    </div>
-  );
-}
-
-function AnimatedArrows({ ...props }) {
-  return (
-    <div className={styles.arrows} {...props}>
-      <div style={{ padding: '10px' }}>
-        <span />
-        <span />
-      </div>
-    </div>
-  );
-}
-
-function AnimatedMouse({ button, ...props }) {
-  let style = null;
-  if (button === 'right') {
-    style = {
-      flexDirection: 'column',
-      paddingTop: '0px',
-      paddingBottom: '20px',
-      marginRight: '20px'
-    };
-  }
-  const rotation = button === 'right' ? [0, 180] : [-90, 90];
-  return (
-    <div className={`${styles.mouseContainer} ${styles.centerContent}`} style={style} {...props}>
-      <AnimatedArrows style={{ transform: `rotate(${rotation[0]}deg)` }} />
-      <div className={styles.mouse}>
-        {button === 'left' && <div className={styles.leftButton} />}
-        {button === 'right' && <div className={styles.rightButton} />}
-        {button === 'scroll' && <div className={styles.mouseButton} />}
-        <div className={styles.bar} />
-        <div className={styles.verticalBar} />
-      </div>
-      <AnimatedArrows style={{ transform: `rotate(${rotation[1]}deg)` }} />
-    </div>
-  );
-}
-
-function MouseDescriptions({
-  button, info, description, keyboardButton
-}) {
-  return (
-    <div className={styles.mouseDescription}>
-      <p className={`${styles.text} ${styles.mouseButtonText}`}>
-        <i>{info}</i>
-        {' '}
-        <br />
-        {' '}
-        { description }
-      </p>
-      {keyboardButton && (
-        <>
-          <KeyboardButton buttonText={keyboardButton} />
-          <p className={styles.plus}>+</p>
-        </>
-      )}
-      <AnimatedMouse button={button} />
-    </div>
-  );
-}
 
 function TourPopup({ setVisibility, isVisible }) {
   const [currentSlide, setCurrentSlide] = useLocalStorageState('currentSlide', 0);
