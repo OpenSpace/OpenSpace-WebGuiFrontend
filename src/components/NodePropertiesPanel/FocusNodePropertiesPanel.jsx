ca223e0f0c098379e537cc5c51a95e5ba79ad78b
Ylva Selling <ylva.selling@gmail.com>
2023-05-23 15:08:16 -0400

    Lint fix focusnodepropertiespanel & rewrite to funtional component

--- src/components/NodePropertiesPanel/FocusNodePropertiesPanel.jsx
+++ src/components/NodePropertiesPanel/FocusNodePropertiesPanel.jsx
@@ -1,34 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { NavigationAnchorKey, ScenePrefixKey } from '../../api/keys';
 
 import NodePropertiesPanel from './NodePropertiesPanel';
 
-class FocusNodePropertiesPanel extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { node } = this.props;
-    return (
-      <NodePropertiesPanel uri={node} isFocusNodePanel />
-    );
-  }
+function FocusNodePropertiesPanel() {
+  const node = useSelector((state) => {
+    const anchorProp = state.propertyTree.properties[NavigationAnchorKey];
+    return anchorProp ? ScenePrefixKey + anchorProp.value : '';
+  });
+  return (
+    <NodePropertiesPanel uri={node} isFocusNodePanel />
+  );
 }
-
-const mapStateToProps = (state) => {
-  const anchorProp = state.propertyTree.properties[NavigationAnchorKey];
-  const anchorNodeURI = anchorProp ? ScenePrefixKey + anchorProp.value : '';
-
-  return {
-    node: anchorNodeURI
-  };
-};
-
-FocusNodePropertiesPanel = connect(
-  mapStateToProps,
-)(FocusNodePropertiesPanel);
 
 export default FocusNodePropertiesPanel;
