a513d7bd0f250c69c345fe239218c91edd004d9c
Ylva Selling <ylva.selling@gmail.com>
2023-05-23 17:55:05 -0400

    Add comment to explain gettingstartedcontext

--- src/components/GettingStartedTour/GettingStartedContext.jsx
+++ src/components/GettingStartedTour/GettingStartedContext.jsx
@@ -4,12 +4,17 @@ import contents from './GettingStartedContent.json';
 
 const RefsContext = React.createContext();
 
+// This context provides refs to the components that are shown in the
+// Getting Started Tour. They are stored in a context so they can
+// be inserted into the context from wherever.
+// They are stored in one ref which stores an object with all keys
+// and their assigned html element
 function RefsProvider(props) {
   const allRefKeys = {};
-  contents.map((content) => {
+  contents.forEach((content) => {
     if (content.key) {
-      content.key.map((key) => {
-        if (!allRefKeys.hasOwnProperty(key)) {
+      content.key.forEach((key) => {
+        if (!Object.keys(allRefKeys).includes(key)) {
           allRefKeys[key] = undefined;
         }
       });
