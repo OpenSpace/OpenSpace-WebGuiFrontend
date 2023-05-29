4d611187b6da7229b189098353f9d68f05d90dea
Emma Broman <emma.broman@liu.se>
2023-05-29 18:44:12 +0200

    Write ColorPicker as functional component

--- src/components/BottomBar/TfEditor/containers/ColorPicker.jsx
+++ src/components/BottomBar/TfEditor/containers/ColorPicker.jsx
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ChromePicker } from 'react-color';
 import PropTypes from 'prop-types';
 
-class ColorPicker extends Component {
-  constructor(props) {
-    super(props);
+function ColorPicker({ onColorChange }) {
+  const [color, setColor] = React.useState('#fff');
 
-    this.state = {
-      color: '#fff'
-    };
+  function handleChangeComplete(color) {
+    setColor(color.hex);
+    onColorChange(color.hex);
   }
 
-  handleChangeComplete(color) {
-    this.setState({ color: color.hex });
-    this.props.onColorChange(color.hex);
-  }
-
-  render() {
-    return (
-      <ChromePicker
-        disableAlpha
-        {...this.props}
-        color={this.state.color}
-        onChangeComplete={(color) => this.handleChangeComplete(color)}
-      />
-    );
-  }
+  return (
+    <ChromePicker
+      disableAlpha
+      {...this.props}
+      color={this.state.color}
+      onChangeComplete={(color) => handleChangeComplete(color)}
+    />
+  );
 }
+
 ColorPicker.propTypes = {
   onColorChange: PropTypes.func.isRequired
 };
+
 export default ColorPicker;
