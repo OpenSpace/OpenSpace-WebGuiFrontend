a10b08529fba24835ca74a280100d757f1b38034
Ylva Selling <ylva.selling@gmail.com>
2023-06-08 11:35:13 -0400

    Change MaterialIcon to Icon FocusEntry & OriginPicker

--- src/components/BottomBar/Origin/OriginPicker.jsx
+++ src/components/BottomBar/Origin/OriginPicker.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Icon } from '@iconify/react';
 /* eslint-disable import/no-webpack-loader-syntax */
 import Aim from 'svg-react-loader?name=Aim!../../../icons/aim.svg';
 import Anchor from 'svg-react-loader?name=Anchor!../../../icons/anchor.svg';
@@ -35,7 +36,6 @@ import {
 import Button from '../../common/Input/Button/Button';
 import Checkbox from '../../common/Input/Checkbox/Checkbox';
 import LoadingString from '../../common/LoadingString/LoadingString';
-import MaterialIcon from '../../common/MaterialIcon/MaterialIcon';
 import Popover from '../../common/Popover/Popover';
 import SettingsPopup from '../../common/SettingsPopup/SettingsPopup';
 import SmallLabel from '../../common/SmallLabel/SmallLabel';
@@ -274,7 +274,7 @@ function OriginPicker() {
         role="button"
         tabIndex={0}
       >
-        <MaterialIcon className={styles.Icon} icon="airplanemode_inactive" />
+        <Icon className={styles.Icon} icon="material-symbols:airplanemode-inactive" />
         <div className={Picker.Title}>
           <span className={Picker.Name}>
             <LoadingString loading={anchor === undefined}>
