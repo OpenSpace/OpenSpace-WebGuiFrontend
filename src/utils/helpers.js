b1a9d4098d75a6a41bb73957ad76467af6321c72
Emma Broman <emma.broman@liu.se>
2023-08-14 21:50:32 +0200

    Feature/improve node meta (#154)
    * Improve information in Node Meta panel to be more clear
    
    * Some more style updates, related to the two tabs
    
    * Fix asset path copying so that it works on CEF GUI, not only in browsers
    
    * Apply suggestions from code review
    
    Co-authored-by: Ylva Selling <ylva.selling@gmail.com>
    
    ---------
    
    Co-authored-by: Ylva Selling <ylva.selling@gmail.com>

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -51,3 +51,34 @@ export function stopEventPropagation(e) {
     if (event.stopPropagation) event.stopPropagation();
   }
 }
+
+/**
+ * Copy a text to the clipboard
+ * @param text - text to copy
+ */
+export function copyTextToClipboard(text) {
+  // Note: Implementation is kind of ugly, but works with CEF.
+  // (The preferred "navigator.clipboard.writeText" only works over https)
+  const textArea = document.createElement('textarea');
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  textArea.style.padding = 0;
+
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+
+  textArea.select();
+  document.execCommand('copy');
+
+  document.body.removeChild(textArea);
+}
