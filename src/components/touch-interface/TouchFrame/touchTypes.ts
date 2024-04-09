739a885284d4e1e04a0745c287d3664d2a70f24a
Dennis <dennis.moradkhani@gmail.com>
2024-04-09 14:23:10 -0600

    add types for flightController topic input state

--- src/components/touch-interface/TouchFrame/touchTypes.ts
+++ src/components/touch-interface/TouchFrame/touchTypes.ts
@@ -3,11 +3,15 @@
 export type InputState = {
   values: {
     zoomIn?: number;
+    zoomOut?: number;
     orbitX?: number;
     orbitY?: number;
     panX?: number;
     panY?: number;
     localRollX?: number;
+    localRollY?: number;
+    globalRollX?: number;
+    globalRollY?: number;
   };
 };
 
