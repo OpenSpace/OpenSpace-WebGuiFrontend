95ed7fb5853d27d2b879f6f409f3101a018e03ce
Emma Broman <emma.broman@liu.se>
2024-07-17 13:57:19 +0200

    Small Node Popover Menu Refactor (#190)
    * Remove concept of a specific "FocusNodePanel" in addition to NodePropertyPopover
    
    Added a lot of complexity for just a difference in window title
    
    * Simplify / localize meta and properties popover code a bit
    
    * Fix contant rerendering of NodePropertiesPanel
    
    * Fix constnt rerendering of NodeMetaPanel
    
    * Apply suggestions from code review

--- src/components/NodePropertiesPanel/NodePopOverContainer.jsx
+++ src/components/NodePropertiesPanel/NodePopOverContainer.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import FocusNodePropertiesPanel from './FocusNodePropertiesPanel';
 import NodePropertiesPanel from './NodePropertiesPanel';
 
 import styles from './NodePopOverContainer.scss';
 
 function NodePopOverContainer() {
-  const activeNodePropertyPanels = useSelector((state) => {
-    const activePanels = state.local.popovers.activeNodePropertyPanels;
-    return Object.keys(activePanels).map((key) => key);
-  });
+  const activePanels = useSelector((state) => state.local.popovers.activeNodePropertyPanels);
+  const activeNodePropertyPanels = Object.keys(activePanels).map((key) => key);
 
   return (
     <div className={styles.nodePopOverContainer}>
-      <FocusNodePropertiesPanel />
       {
         activeNodePropertyPanels.map((uri) => <NodePropertiesPanel uri={uri} key={uri} />)
       }
